876f0eb14a4400abd8043f8811564747364c389a
ObeDolapo <dolapo@lotusbetaanalytics.com>
2017-06-30 10:22:48 +0100

    my calculator in anglarJS

--- nscript.js
+++ nscript.js
@@ -1,27 +1,56 @@
-function c(val)
-  {
-    document.getElementById("display").value = val;
-  }
-  
-  function math(val) 
-  {
-    document.getElementById("display").value += val;
-  
-  }
-  function e()
-  {
-    try
-    {
-		c(eval(document.getElementById("display").value))
-    }
-    catch (e)
-    {
-		c('Error')
-    }
+
+var app = angular.module('calculatorApp', []);
+app.controller('calculatorBtn', function($scope){
+   	$scope.title = "The Calculator";
+   	$scope.proprietary = "Adedolapo";
+   	$scope.display = "";
+
+$scope.onScreen = function(x) {
+   	$scope.display +=x;
+
 }
 
-  function bs() 
-   {
-	   var value = document.getElementById("display").value; 
-	   document.getElementById("display").value = value.substr(0, value.length - 1);
-   }
+$scope.equal = function ()  {
+	if ($scope.display !='') {
+	x = $scope.display;
+	x = eval (x); 
+	$scope.display=x;
+	}
+}
+
+$scope.inverseOf = function(){
+
+	x=$scope.display;
+	x = 1/x;
+	$scope.display=x;
+
+}
+
+
+$scope.powerOfTwo=function() {
+
+	$scope.display=eval($scope.display*$scope.display);
+
+}
+
+$scope.plusminus = function () {
+	x=$scope.display;
+	x=(x)*-1;
+	x=$scope.display=x;
+}
+
+$scope.clearOne = function(){
+	
+	var screenNum = $scope.display;
+	var len= screenNum.length-1;
+	var backOne = screenNum.substring(0,len);
+	$scope.display = backOne;
+}
+$scope.clearIt = function() {
+	$scope.display= " ";
+
+}
+
+
+});
+
